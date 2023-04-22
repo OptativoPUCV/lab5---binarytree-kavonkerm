c9962ed1ecfa6239173ac01d00d3b1d97cfa856d
Replit user <>
2023-04-22 18:41:49 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -102,7 +102,6 @@ void removeNode(TreeMap * tree, TreeNode* node){
     }
     free(node);
   }
-  
   if (node->left == NULL || node->right == NULL){ //1 hijo
     TreeNode *hijo;
     if (node->left != NULL) hijo = node->left;
@@ -113,25 +112,11 @@ void removeNode(TreeMap * tree, TreeNode* node){
     }
     else if (node == node->parent->left){
       node->parent->right = hijo;
-      free(node);
     }
     else{
       node->parent->left = hijo;
-      free(node);
     }
-  }
-  if (node->left != NULL && node->right != NULL){ //2 hijos
-    TreeNode *hijoMen = node->left;
-    if (hijoMen->left == NULL && hijoMen->right == NULL){
-      node->parent->right = node->right;
-      hijoMen = NULL;
-      free(node);
-    }
-    else{
-      node->parent->right = hijoMen->right;
-      hijoMen = NULL;
-      free(node);
-    }
+    free(node)
   }
 }
 
